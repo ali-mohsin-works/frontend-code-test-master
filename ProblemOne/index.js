cae21f8652b445d70d365f84524498bde2655be3
ali-mohsin-works <alimohsin.works@gmail.com>
2020-06-19 18:23:43 -0400

    ProblemOne - Task4

--- ProblemOne/index.js
+++ ProblemOne/index.js
@@ -59,7 +59,39 @@ function mutateArray(a) {
     return a;
 }
 
+var newarr = [];
+for(var k in arr) {
+  var item = arr[k];   
+  var item_sum = 0;
+    for(var j in item.guest_booking.some_array) {
+      item_sum += item.guest_booking.some_array[j];
+    }
+  if(item.guest_type == "guest"){
+    newarr.push({ 
+      "guest_type" : item.guest_type, 
+      "first_name" :  item.first_name, 
+      "last_name" :  item.last_name,
+      "room_no" :  item.guest_booking.room_no,
+      "some_total" : item_sum
+    });
+  }
+}
+
+function sortByProperty(last_name,first_name ){  
+  return function(a,b){  
+     if(a[last_name] > b[last_name] || a[first_name] > b[first_name])  
+        return 1;  
+     else if(a[last_name] < b[last_name] || a[first_name] < b[first_name])  
+        return -1;  
+ 
+     return 0;  
+  }  
+}
+
+newarr.sort(sortByProperty("last_name","first_name"));
+
 $(document).ready(function() {
     $('#originalArray').html(JSON.stringify(arr, null, 2));
-    $('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+    //$('#resultsArray').html(JSON.stringify(mutateArray(arr), null, 2));
+    $('#resultsArray').html(JSON.stringify(mutateArray(newarr), null, 2));
 });
